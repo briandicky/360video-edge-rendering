8c084f7d7b67a0e4a1301a05895622966e33b16a
MosQuito <briandicky@gmail.com>
2018-03-06 19:08:07 +0800

    Add an orientation transformer

---
+++ dash/ori_2_tiles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#   Program:
+#       To repackage and generate the equirectangular video from tiled videos.
+#   Author:
+#       Wen-Chih, MosQuito, Lo
+#   Date:
+#       2017.3.6
+
+import os 
+import math
+
+yaw = float(sys.argv[1])
+pitch = float(sys.argv[2])
