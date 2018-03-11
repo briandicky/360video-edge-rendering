6869eb379f0785f5ae3a776e7f24385ef24c7e39
MosQuito <briandicky@gmail.com>
2018-03-11 22:33:05 +0800

    Fix bugs

--- dash/libs/server.py
+++ dash/libs/server.py
@@ -27,23 +27,21 @@ while True:
     # Wait for a connection 
     print >> sys.stderr, 'waiting for a connection' 
     connection, client_address = sock.accept()
+    try:
+        print >> sys.stderr, 'connection from', client_address
 
-try:
-    print >>sys.stderr, 'connection from', client_address
-
-    # Receive the data in small chunks and retransmit it
-    while True:
-        data = connection.recv(16)
-        print >>sys.stderr, 'received "%s"' % data
-        
-        if data:
-            print >>sys.stderr, 'sending data back to the client'
-            connection.sendall(data)
-        else:
-            print >>sys.stderr, 'no more data from', client_address
-            break
-
-finally:
-    # Clean up the connection
-    connection.close()
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print >> sys.stderr, 'received "%s"' % data
+            
+            if data:
+                print >> sys.stderr, 'sending data back to the client'
+                connection.sendall(data)
+            else:
+                print >> sys.stderr, 'no more data from', client_address
+                break
+    finally:
+        # Clean up the connection
+        connection.close()
 
