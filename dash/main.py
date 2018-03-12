7e1cbe747c23809e1de08be851c0d98265609ac2
MosQuito <briandicky@gmail.com>
2018-03-12 22:04:01 +0800

    Add constants

--- dash/main.py
+++ dash/main.py
@@ -9,37 +9,34 @@
 import os 
 import sys
 import math
+import socket
+import time
 from libs import tile_packger 
 from libs import cal_prob
 
-# viewing domain arguments
-yaw = float(sys.argv[1])
-pitch = float(sys.argv[2])
+# viewing constants
+#yaw = 
+#pitch = 
+#roll =
 fov_degreew = 100
 fov_degreeh = 100
 tile_w = 3
 tile_h = 3
 
-print("yaw: %s" % yaw)
-print("pithc: %s" % pitch)
-print("fov_degreew: %s" % fov_degreew)
-print("fov_degreeh: %s" % fov_degreeh)
-print("tile_w: %s" % tile_w)
-print("tile_h: %s" % tile_h)
-print
+# socket constants
+SERVER_ADDR = "140.114.77.125"
+SERVER_PORT = 9487
+CHUNK_SIZE = 4096
 
-# Compressed domain arguments
-no_of_tiles = tile_w*tile_h
-ip_encoding_server = "140.114.77.125"
+# compressed domain constants
+NO_OF_TILES = tile_w*tile_h
 seg_length = 10
-seg_id = int(sys.argv[3])
+seg_id = 3
 
-print("no_of_tiles: %s" % no_of_tiles)
-print("seg_length: %s" % seg_length)
-print("seg_id: %s" % seg_id)
-
+f = open("record.csv", "w")
+f.write("serverip,serverport,serverts,clientip,clientport,clientts,segid,rawYaw,rawPitch,rawRoll\n")
+# End of constants
 
 #viewed_tiles = tile_packger.ori_2_tiles(yaw, pitch, fov_degreew, fov_degreeh, tile_w, tile_h)
 #tile_packger.mixed_tiles_quality(no_of_tiles, seg_length, seg_id, [], [], viewed_tiles)
-
 
