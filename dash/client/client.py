50e924e827911cb915b02caa3e24873e5ba2b7f2
MosQuito <briandicky@gmail.com>
2018-03-12 16:45:09 +0800

    More communication info between server and client

--- dash/client/client.py
+++ dash/client/client.py
@@ -8,11 +8,18 @@
 
 import socket 
 import sys 
+import time
 
+# Constants
 SERVER_ADDR = "140.114.77.125"
 SERVER_PORT = 9487
 
 CHUNK_SIZE = 4096
+
+yaw = 30.0000583333
+pitch = 3.24956388889
+roll = -21.4974361111
+# End of constants
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,13 +31,14 @@ sock.connect(server_address)
 
 try:
     # Send data
-    message= 'This is the message.  It will be repeated.\n'
-    print >> sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    ori = (time.time(), yaw, pitch, roll)
+    print >> sys.stderr, 'sending (%s, %s, %s, %s)' % ori
+    mes = str(ori[0]) + "," + str(ori[1]) + "," + str(ori[2]) + "," + str(ori[3])
+    sock.sendall(mes)
 
     # Look for the response
     amount_received = 0
-    amount_expected = len(message)
+    amount_expected = len(mes)
 
     while amount_received < amount_expected:
         data = sock.recv(CHUNK_SIZE)
--- dash/libs/server.py
+++ dash/libs/server.py
@@ -8,13 +8,17 @@
 
 import socket
 import sys 
+import time
 
+# Constants
 SERVER_ADDR = "140.114.77.125"
 SERVER_PORT = 9487
 
 CHUNK_SIZE = 4096
 
-f = open("record.csv", "a")
+f = open("record.csv", "w")
+f.write("serverip,serverport,serverts,clientip,clientport,clientts,rawYaw,rawPitch,rawRoll\n")
+# End of constants
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,19 +33,24 @@ sock.listen(1)
 
 while True:
     # Wait for a connection 
-    print >> sys.stderr, 'waiting for a connection' 
+    print >> sys.stderr, 'waiting for a connection...' 
     connection, client_address = sock.accept()
     try:
-        print >> sys.stderr, 'connection:', connection
+        #print >> sys.stderr, 'connection:', connection
         print >> sys.stderr, 'connection from', client_address
 
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(CHUNK_SIZE)
             print >> sys.stderr, 'received "%s"' % data
-            f.write(data)
             
             if data:
+                f.write(str(SERVER_ADDR) + ",")
+                f.write(str(SERVER_PORT) + ",")
+                ts = time.time()
+                f.write(str(ts) + ",")
+                f.write(str(client_address[0]) + "," + str(client_address[1]) + ",")
+                f.write(data)
                 print >> sys.stderr, 'sending data back to the client'
                 connection.sendall(data)
             else:
