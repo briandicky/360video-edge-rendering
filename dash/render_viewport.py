08dae1f3562f4952b35e6a3d4efb0036561844c2
MosQuito <briandicky@gmail.com>
2018-03-23 18:20:40 +0800

    left and right viewport

--- dash/render_viewport.py
+++ dash/render_viewport.py
@@ -3,7 +3,7 @@ import cv2
 import math
 from PIL import Image
 
-YAW     = -120.03605833333
+YAW     = -1.03605833333
 PITCH   = 0.103563888889
 ROLL    = -3.993
 
@@ -36,7 +36,7 @@ def video_2_image(path):
         # save frame as PNG format
         cv2.imwrite("./frame/frame%d.png" % count, frame)
         success, frame = vidcap.read()
-        print "Read a new frame: %s", count
+        print "Read a new frame:", count
         count += 1 
 
 def ori_2_fixation(yaw, pitch):
@@ -45,8 +45,6 @@ def ori_2_fixation(yaw, pitch):
         yaw = yaw % (-180)
     elif yaw < (-180):
         yaw = yaw % 180
-    else:
-        print("Should not be here.")
 
     # pitch boundary correction
     if pitch > 90:
@@ -54,23 +52,38 @@ def ori_2_fixation(yaw, pitch):
         if yaw > 0:
             yaw = yaw - 180
         else:
-            yaw = yaw + 180
+            yaw = 180 + yaw
     elif pitch < (-90):
-        pitch = (-180) - pitch
+        pitch = 0 - 180 - pitch
         if yaw > 0:
             yaw = yaw - 180
         else:
-            yaw = yaw + 180
-    else:
-        print("Should not be here.")
+            yaw = 180 + yaw
 
     x = RADIUS * yaw * math.pi / 180
     y = RADIUS * math.tan(math.radians(pitch))
-    fixation = [x, y]
+
+    # normalization
+    if y > 959:
+        y = 959
+    if y < (-959):
+        y = (-959)
+    x_ori = x + 1920
+    y_ori = (0 - y) + 960
+
+    # left and right fixation point
+    # the distance of left and right pupil is 65mm
+    left_x_ori = x_ori - 50
+    right_x_ori = x_ori + 50
+
+    fixation = []
+    fixation.append([round(left_x_ori), round(y_ori)])
+    fixation.append([round(right_x_ori), round(y_ori)])
     return fixation
 
 if __name__ == "__main__":
     # create folder and clip video into frames
     video_2_image('./video_low_4s.mp4')
 
-    ori_2_fixation(YAW, PITCH)
+    fixation = ori_2_fixation(YAW, PITCH)
+    print(fixation)
