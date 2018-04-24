f48f4d055cb41b29fcd49d53e1a7af318a2359c5
MosQuito <briandicky@gmail.com>
2018-04-24 13:31:13 +0800

    download tiled videos based on NO_OF_TILES

--- libs/tiled.py
+++ libs/tiled.py
@@ -85,7 +85,7 @@ def mixed_tiles_quality(no_of_tiles, seg_length, seg_id, VIDEO,
 
     # download the videos from encoding server
     req_ts = time.time()
-    download_tiled_video_from_server(VIDEO, seg_length, video_list)
+    download_tiled_video_from_server(VIDEO, no_of_tiles, seg_length, video_list)
     recv_ts = time.time()
     
     # Concatenate init track and each tiled tracks
@@ -149,7 +149,7 @@ def only_fov_tiles(no_of_tiles, seg_length, seg_id, VIDEO,
                                                                      
     # download the videos from encoding server
     req_ts = time.time()
-    download_tiled_video_from_server(VIDEO, seg_length, video_list)
+    download_tiled_video_from_server(VIDEO, no_of_tiles, seg_length, video_list)
     recv_ts = time.time()
 
     # Concatenate init track and each tiled tracks
@@ -198,10 +198,14 @@ def only_fov_tiles(no_of_tiles, seg_length, seg_id, VIDEO,
     return (req_ts,recv_ts)
 
 
-def download_tiled_video_from_server(VIDEO, seg_length, video_list=[]):
+def download_tiled_video_from_server(VIDEO, no_of_tiles, seg_length, video_list=[]):
     for line in video_list:
-        tile = ENCODING_SERVER_ADDR + bitrate_path[1:] + str(seg_length) + "s" + "/" + VIDEO + auto_path + line
-        #print >> sys.stderr, "Downloadin %s ..." % tile
+        if no_of_tiles == 3*3:
+            tile = ENCODING_SERVER_ADDR + bitrate_path[1:] + str(seg_length) + "s" + "/" + VIDEO + auto_path + "3x3/" + line
+        elif no_of_tiles == 5*5:
+            tile = ENCODING_SERVER_ADDR + bitrate_path[1:] + str(seg_length) + "s" + "/" + VIDEO + auto_path + "5x5/" + line
+        else:
+            print >> sys.stderr, "Wrong NO_OF_TILES, please check it out."
 
         try:
             rm_tile = tmp_path + line
