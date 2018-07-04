c06595cef9559845b24cb79de4824ad29f98367a
MosQuito <briandicky@gmail.com>
2018-07-05 00:52:51 +0800

    upload rtt files

---
+++ simulator/aws_client_2_edge/rtt.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# virginia
+ping -c 1000 54.173.80.43 2>&1 | tee -a tokyo_2_virginia_rtt.csv &
+
+# california
+ping -c 1000 18.144.29.198 2>&1 | tee -a tokyo_2_california_rtt.csv &
+
+# motreal
+ping -c 1000 35.183.47.103 2>&1 | tee -a tokyo_2_montreal_rtt.csv &
+
+# frankfurt
+ping -c 1000 18.185.126.92 2>&1 | tee -a tokyo_2_frankfurt_rtt.csv &
+
+# london
+ping -c 1000 35.178.82.7 2>&1 | tee -a tokyo_2_london_rtt.csv &
+
+# Saopaulo
+ping -c 1000 18.228.24.236 2>&1 | tee -a tokyo_2_saopaulo_rtt.csv &
---
+++ simulator/simulator.py
@@ -0,0 +1,19 @@
+import os 
+import sys
+import math
+
+### Libraries ###
+
+
+### Golbal Variables ###
+totalTime = 3 * 24 * 60 * 60    # 3 days time
+
+
+### Functions ###
+def main():
+
+
+
+### Main ###
+if __name__ == '__main__':
+    main()
